7025e992cf1696109fdf08ec0dc3d45de1509d32
Aaron Gullickson <aarong@uoregon.edu>
2018-07-17 10:40:31 -0700

    changed analysis.R to testing.R

---
+++ testing.R
@@ -0,0 +1,4 @@
+#This is just an empty R script that you can use for testing out code. 
+#All of the final R code will need to be incorporated into your R Markdown file.
+
+load("input/addhealth.RData")
